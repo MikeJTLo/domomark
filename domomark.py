b5605e2f2ad4cbcfdd86687c7eef08075903dc1e
Michael Lo <lo.michael@gmail.com>
2013-07-24 13:35:12 -0400

    Integrating jinja2

--- domomark.py
+++ domomark.py
@@ -1,8 +1,12 @@
-import os
+import os 
 from flask import Flask
+from jinja2 import Environment, PackageLoader
 
 app = Flask(__name__)
+env = Environment(loader=PackageLoader('domomark','templates'))
+template = env.get_template('testone.html')
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    #return 'Hello World!'
+    print template.render()
