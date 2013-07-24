bea95d34ec6e70d0bee15339b4be8ebbfed8f8a0
Michael Lo <lo.michael@gmail.com>
2013-07-24 13:41:24 -0400

    HTML5 BoilerPlate Additions into the Templates folder

--- domomark.py
+++ domomark.py
@@ -4,7 +4,7 @@ from jinja2 import Environment, PackageLoader
 
 app = Flask(__name__)
 env = Environment(loader=PackageLoader('domomark','templates'))
-template = env.get_template('testone.html')
+template = env.get_template('index.html')
 
 @app.route('/')
 def hello():
