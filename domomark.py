bcec7f3d62c899c206a47d638cd5a6686197e8e1
Michael Lo <lo.michael@gmail.com>
2013-07-26 15:41:22 -0400

    Add 960.gs css

--- domomark.py
+++ domomark.py
@@ -6,9 +6,32 @@ app = Flask(__name__)
 env = Environment(loader=PackageLoader('domomark','templates'))
 template = env.get_template('index.html')
 
+n = 1000000000
+outputString = 'Print a Fibonacci series up to %d: ' % n
+a, b = 0, 1
+
+def testFib(n):
+	global outputString	
+	global a, b
+	while b<n:
+		outputString += '%d, ' % b 
+		a, b = b, a+b
+	return outputString
+
+
 @app.route('/')
 def hello():
-    #return 'Hello World!'
-    return "This is a work in progress."
-    #return template.render()
-    #return urllib2.geturl()
+	global n
+	testFib(n)
+	return outputString
+    #return template.render(outputString)
+    
+    
+@app.route('/Test')
+def test():
+	return "Test Page"
+    #return template.render(outputString)
+    
+@app.route('/Two')
+def test2():
+    return template.render(title="Project Domomark")
